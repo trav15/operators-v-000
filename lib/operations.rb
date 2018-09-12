20234fc8d16c048928992dd56b2c20ea47e9ae3f
Travis T <travistokuyama@gmail.com>
2018-09-12 18:48:26 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-  speed < 40 ? true : false 
-  speed > 60 ? true : false 
+  if speed < 40
+    return true
+  elsif speed > 60 
+    return true 
+  else  
+    return false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 ? true : false 
+  speed > 60 ? true : false 
 end
 	
 
